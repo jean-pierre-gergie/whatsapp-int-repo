231a5ecdf0ea07c5f019fcc4c6814d02caea1906
jean-pierre-gergie <60657641+jean-pierre-gergie@users.noreply.github.com>
2024-08-12 14:58:26 +0300

    1hour token

--- website/app/config.py
+++ website/app/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = '[Wfrs!HR7B<^NL>'
     JWT_SECRET_KEY = 'your_jwt_secret_key' 
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=0.02)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     API_KEY = 'iHB20OJavj7OcHzCmfIKyCHlAK'
     MONGODB_URI = 'mongodb://mongodb_container:27017/' 
     MONGODB_NAME = 'whatsapp_data'
