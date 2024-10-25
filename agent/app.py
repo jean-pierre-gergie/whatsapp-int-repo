ef71b952009911e432b6df5d778d3c30c75b3c74
jean-pierre-gergie <60657641+jean-pierre-gergie@users.noreply.github.com>
2024-10-25 16:22:47 +0300

    adfad

--- agent/app.py
+++ agent/app.py
@@ -44,7 +44,7 @@ socketIO_URL = os.getenv('CHAT_AGENT_URL')
 
 @app.route('/agent_server')
 def index():
-    return render_template('index.html', socket_url=socketIO_URL)
+    return render_template('index.html', socket_url='/agent/socket.io')
 
 @socketio.on('connect', namespace='/agent_namespace')
 def connect():
