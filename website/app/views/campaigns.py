4a2be88a1663ea6845f3d0f80dec654e1c5fb904
jean-pierre-gergie <60657641+jean-pierre-gergie@users.noreply.github.com>
2024-10-30 10:36:56 +0200

    set cookies for croos domain

--- website/app/views/campaigns.py
+++ website/app/views/campaigns.py
@@ -610,5 +610,5 @@ def dynamic_redirect():
     agent_server_url = os.getenv('CHAT_AGENT_SERVER_URL')
     logger.info(f"redirecting to {agent_server_url}")
     response = make_response(redirect(agent_server_url))
-    response.set_cookie('jwt_token', token, httponly=False, secure=True, samesite='Strict')
+    response.set_cookie('jwt_token', token, httponly=False, secure=True, samesite='Lax' ,domain='www.ocmymada.com')
     return response
