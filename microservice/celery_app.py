9c754b5851f7abb14a13bf93368d586038dcb199
jean-pierre-gergie <60657641+jean-pierre-gergie@users.noreply.github.com>
2024-12-16 13:28:18 +0200

    larger delay

--- microservice/celery_app.py
+++ microservice/celery_app.py
@@ -56,12 +56,12 @@ celery_app = Celery(
 celery_app.conf.beat_schedule = {
     'refresh-webhook-jwt-token': {
         'task': 'tasks.refresh_webhook_jwt_token',
-        'schedule': crontab( minute='*/10'), 
+        'schedule': crontab( minute='*/50'), 
         'options': {'catchup': False},
     },
     'refresh-api-key-weekly': {
         'task': 'tasks.refresh_api_key',
-        'schedule':  crontab(minute="*/20"),
+        'schedule':  crontab(minute="*/51"),
         'options': {'catchup': False},    
     },
 }
